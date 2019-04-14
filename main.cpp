19470ef36905c83834c713ce757a14607452df1f
yulyashehov <yulyashehov@gmail.com>
2019-04-14 21:34:11 +0400

    Создана пустая программа на c++

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitLaba
+//
+//  Created by Юлия Шеховцова on 14/04/2019.
+//  Copyright © 2019 Julia Shekhovtsova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
